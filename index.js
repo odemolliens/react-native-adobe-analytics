7b30a812f0fdf7e955ae3498e736edcffe965b19
Imran Mentese <Imran.Mentese@arhs-spikeseed.com>
2018-04-05 11:16:54 +0200

    clear code

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
 // @flow
-import withTrackEvent from './src/components/HOC/event.hoc';
-import ScreenTrackingMiddleware from './src/middlewares/screen.middleware';
-import { getDisplayName, getScreenNameForTracking } from './src/utils/functions';
-import { EventParametersPropTypes } from './src/utils/definitions';
-import { EventDetailParameter } from './src/utils/definitions';
-import { ScreenUtils } from './src/middlewares/screen_utils.middleware';
 import Adobe from './src/adobe';
 
 export {
-  getDisplayName,
-  getScreenNameForTracking,
-  ScreenTrackingMiddleware,
-  Adobe,
-  withTrackEvent,
-  EventParametersPropTypes,
-  EventDetailParameter,
-  ScreenUtils
+  Adobe
 };
